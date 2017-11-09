8e4e14b885ad757e85b460ae0810fcb7428b33c4
pkrisz0 <p.krisz89@gmail.com>
2017-11-09 16:53:14 +0100

    chatapp add testuser - fixed bug - 4

--- src/main/java/com/greenfox/pkrisz0/chatapp/controller/MainController.java
+++ src/main/java/com/greenfox/pkrisz0/chatapp/controller/MainController.java
@@ -4,6 +4,7 @@ import com.greenfox.pkrisz0.chatapp.model.User;
 import com.greenfox.pkrisz0.chatapp.repository.ChatAppRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,8 +14,9 @@ public class MainController {
     ChatAppRepo chatAppRepo;
 
     @GetMapping({"", "/"})
-    public String index(){
+    public String index(Model model){
         chatAppRepo.save(new User("krisz","12345"));
+        model.addAttribute("user", chatAppRepo.findOne(1));
         return "main";
     }
 }

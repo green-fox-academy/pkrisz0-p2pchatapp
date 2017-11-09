33afc985d73d509cac781c3f3cc65a676cbab2bc
pkrisz0 <p.krisz89@gmail.com>
2017-11-09 16:40:16 +0100

    chatapp add testuser - fixed bug - 3

--- src/main/java/com/greenfox/pkrisz0/chatapp/model/User.java
+++ src/main/java/com/greenfox/pkrisz0/chatapp/model/User.java
@@ -1,11 +1,9 @@
 package com.greenfox.pkrisz0.chatapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="chatusers")
 public class User {
     String userName;
     String password;
